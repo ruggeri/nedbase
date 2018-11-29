1aef2d0de6f2ed56ca6c99f45d2d6d0ab86aa821
Ned Ruggeri <ruggeri@self-loop.com>
2018-11-29 01:32:56 -0800

    add ReadGuard timed try_to_acquire methods

--- src/locking/read_guards.rs
+++ src/locking/read_guards.rs
@@ -14,15 +14,27 @@ impl<'a> NodeReadGuard<'a> {
   pub fn acquire(btree: &'a BTree, identifier: &str) -> NodeReadGuard<'a> {
     ::util::thread_log(&format!("trying to acquire read lock on node {}", identifier));
     let lock = btree.get_node_arc_lock(&identifier);
-    let guard = NodeReadGuard::acquire_from_lock(lock);
+    let node = lock.read();
     ::util::thread_log(&format!("acquired read lock on node {}", identifier));
+    let guard = NodeReadGuard { lock: Arc::clone(&lock), node };
 
-    guard
+    unsafe {
+      mem::transmute(guard)
+    }
   }
 
-  pub fn acquire_from_lock(lock: Arc<RwLock<Node>>) -> NodeReadGuard<'a> {
-    let node = lock.read();
-    let guard = NodeReadGuard { lock: Arc::clone(&lock), node };
+  pub fn try_to_acquire(btree: &'a BTree, identifier: &str) -> Option<NodeReadGuard<'a>> {
+    ::util::thread_log(&format!("trying to acquire read lock on node {}", identifier));
+    let lock = btree.get_node_arc_lock(&identifier);
+    let node = match lock.try_read_for(::std::time::Duration::from_millis(1)) {
+      None => {
+        ::util::thread_log(&format!("abandoned read lock acquisition on node {}", identifier));
+        return None
+      },
+      Some(node_guard) => node_guard
+    };
+    ::util::thread_log(&format!("acquired read lock on node {}", identifier));
+    let guard = Some(NodeReadGuard { lock: Arc::clone(&lock), node });
 
     unsafe {
       mem::transmute(guard)
@@ -47,9 +59,26 @@ impl<'a> RootIdentifierReadGuard<'a> {
     ::util::thread_log("trying to acquire read lock on root identifier");
     let identifier = btree.root_identifier_lock.read();
     ::util::thread_log("acquired read lock on root identifier");
+
     RootIdentifierReadGuard {
       identifier
     }
+  }
+
+  pub fn try_to_acquire(btree: &'a BTree) -> Option<RootIdentifierReadGuard<'a>> {
+    ::util::thread_log("trying to acquire read lock on root identifier");
+    let identifier = match btree.root_identifier_lock.try_read_for(::std::time::Duration::from_millis(1)) {
+      None => {
+        ::util::thread_log("abandoned trying to acquire read lock on root identifier");
+        return None
+      },
+      Some(identifier_guard) => identifier_guard,
+    };
+    ::util::thread_log("acquired read lock on root identifier");
+
+    Some(RootIdentifierReadGuard {
+      identifier
+    })
   }
 }
 
