399c0c3368e2ecda3ff78ff043edb07bedba45a1
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-14 15:02:08 -0800

    LockSet: add temp acquisition

--- src/locking/lock_set/lock_set.rs
+++ src/locking/lock_set/lock_set.rs
@@ -72,6 +72,20 @@ impl LockSet {
     self.acquire_read_guard_for_hold(lock_target)
   }
 
+  fn read_guard_for_temp(&mut self, lock_target: &LockTarget) -> Rc<RefCell<Guard>> {
+    if !self.guards.contains_key(lock_target) {
+      return self.acquire_read_guard_for_temp(lock_target);
+    }
+
+    // Attempt to upgrade the retained lock.
+    if let Some(guard) = self.upgrade_for_read_temp(lock_target) {
+      return guard;
+    }
+
+    // We failed the upgrade. So we'll have to reacquire.
+    self.acquire_read_guard_for_temp(lock_target)
+  }
+
   fn write_guard_for_hold(&mut self, lock_target: &LockTarget) -> Rc<RefCell<Guard>> {
     if !self.guards.contains_key(lock_target) {
       return self.acquire_write_guard_for_hold(lock_target);
@@ -98,6 +112,24 @@ impl LockSet {
         (LockMode::Write, Guard::Write(guard))
       }
     };
+
+    // Next, wrap it in RefCell so that someone can borrow a guard for
+    // mutation. (Though this is a read guard.)
+    let guard = RefCell::new(guard);
+    // Next, wrap in Rc so that user can hold on to as long as they
+    // need.
+    let guard = Rc::new(guard);
+
+    // Store a weak version in the map.
+    self.guards.insert(lock_target.clone(), (lock_mode, Rc::downgrade(&guard)));
+
+    guard
+  }
+
+  fn acquire_read_guard_for_temp(&mut self, lock_target: &LockTarget) -> Rc<RefCell<Guard>> {
+    let lock_mode = LockMode::Read;
+    let guard = ReadGuard::acquire_read_guard(&self.btree, lock_target);
+    let guard = Guard::Read(guard);
 
     // Next, wrap it in RefCell so that someone can borrow a guard for
     // mutation. (Though this is a read guard.)
@@ -178,6 +210,11 @@ impl LockSet {
         Some(guard)
       }
     }
+  }
+
+  fn upgrade_for_read_temp(&mut self, lock_target: &LockTarget) -> Option<Rc<RefCell<Guard>>> {
+    let (_, guard) = &self.guards[lock_target];
+    guard.upgrade()
   }
 
   fn upgrade_for_write_hold(&mut self, lock_target: &LockTarget) -> Option<Rc<RefCell<Guard>>> {
