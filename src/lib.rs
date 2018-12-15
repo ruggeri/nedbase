9ffea046a1ddff4c9b519ba1684e8c5edc86afb1
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-14 23:02:43 -0800

    remove the dependency on rental; add some comments to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 extern crate parking_lot;
 extern crate rand;
-#[macro_use]
-extern crate rental;
 
+// Allow submodules to access the public contents of other submodules.
 pub(self) mod btree;
 pub(self) mod constants;
 pub(self) mod locking;
 pub(self) mod node;
 
 pub use btree::BTree;
+// TODO: I would rather present something named more like a
+// `Transaction` object. The `Transaction` would presumably manage the
+// `LockSet`.
 pub use locking::LockSet;
