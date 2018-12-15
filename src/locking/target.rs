e2089adfa505b7d6c2ee25cb214ff435b26f44d0
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-14 23:59:01 -0800

    add comments for nedbase::locking::{LockTarget, TransactionMode}

--- src/locking/target.rs
+++ src/locking/target.rs
@@ -1,3 +1,6 @@
+// This represents a target for lock acquisition. There are two kinds of
+// locks: RootIdentifier and Node locks. In the case of a Node lock, we
+// specify the identifier, which is a String.
 #[derive(Clone, Eq, Hash, PartialEq)]
 pub enum LockTarget {
   RootIdentifier,
--- src/locking/transaction_mode.rs
+++ src/locking/transaction_mode.rs
@@ -1,3 +1,10 @@
+// TransactionMode determines whether you are allowed to acquire write
+// locks at all. Also, if you *read* a node, in ReadWrite mode we'll
+// have you acquire a *write* lock, because of the possibility that
+// another query in the transaction wants to write that node.
+//
+// TODO: I think this should eventually live in a submodule dedicated to
+// transactions.
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum TransactionMode {
   ReadOnly,
