adb3f9eafd4c7772a902fa4f8ef4edd27439e4cf
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-14 23:56:01 -0800

    rustfmt locking/mod.rs

--- src/locking/mod.rs
+++ src/locking/mod.rs
@@ -7,7 +7,10 @@ mod transaction_mode;
 // TODO: I would like to eliminate exposing primitive guards like this
 // to the world.
 pub use self::guards::{Guard, ReadGuard, WriteGuard};
-pub use self::lock_set::{LockSet, LockSetNodeReadGuard, LockSetNodeWriteGuard, LockSetReadGuard, LockSetRootIdentifierWriteGuard, LockSetWriteGuard};
+pub use self::lock_set::{
+  LockSet, LockSetNodeReadGuard, LockSetNodeWriteGuard,
+  LockSetReadGuard, LockSetRootIdentifierWriteGuard, LockSetWriteGuard,
+};
 pub use self::paths::{ReadGuardPath, WriteGuardPath};
 pub use self::target::LockTarget;
 pub use self::transaction_mode::TransactionMode;
