1b43b2dcd678b2d2853aed83f939507c28d3cc0f
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-15 18:34:46 -0800

    begin writing more sophisticated tx isolation test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,22 +1,46 @@
 extern crate nedbase;
+extern crate rand;
 
 use nedbase::{BTree, LockSet, TransactionMode};
 use std::sync::Arc;
 use std::thread;
 
 const MAX_KEYS_PER_NODE: usize = 1024;
-const NUM_INSERTIONS_PER_THREAD: u32 = 20_000;
+const NUM_KEYS_PER_THREAD: u32 = 10_000;
 const NUM_THREADS: u32 = 32;
 
 fn main() {
+  // Make the BTree.
   let btree = Arc::new(BTree::new(MAX_KEYS_PER_NODE));
+
+  // Make the work.
+  let keyset = {
+    let mut keyset = vec![];
+    for _ in 0..(NUM_THREADS * NUM_KEYS_PER_THREAD) {
+      let key1 = btree.get_new_identifier();
+      let key2 = btree.get_new_identifier();
+
+      let pair = if key1 < key2 {
+        (key1, key2)
+      } else {
+        (key2, key1)
+      };
+
+      keyset.push(pair);
+    }
+
+    keyset
+  };
+
+  let keyset = Arc::new(keyset);
 
   // Spawn a bunch of threads to hammer the BTree.
   let mut join_handles = vec![];
   for _ in 0..NUM_THREADS {
     join_handles.push({
       let btree = Arc::clone(&btree);
-      thread::spawn(move || perform_insertions(&btree))
+      let keyset = Arc::clone(&keyset);
+      thread::spawn(move || run_thread(&btree, keyset))
     });
   }
 
@@ -27,32 +51,46 @@ fn main() {
 }
 
 // A thread's work.
-fn perform_insertions(btree: &Arc<BTree>) {
-  // Make lots and lots of insertions.
-  let mut insertions = vec![];
-  for _ in 0..NUM_INSERTIONS_PER_THREAD {
-    let mut lock_set = LockSet::new(btree, TransactionMode::ReadWrite);
+fn run_thread(btree: &Arc<BTree>, keyset: Arc<Vec<(String, String)>>) {
+  // First, shuffle the keys.
+  let keyset = {
+    let mut rng = thread_rng();
+    let mut keyset = (*keyset).clone();
+    keyset.shuffle(&mut rng);
+    use rand::prelude::*;
 
-    let insertion = btree.get_new_identifier();
-    BTree::optimistic_insert(btree, &mut lock_set, &insertion);
-    insertions.push(insertion.clone());
-  }
+    keyset
+  };
 
-  // Next, check that we can properly find what we have added.
-  for insertion in insertions {
-    let mut lock_set = LockSet::new(btree, TransactionMode::ReadWrite);
+  for (idx, (key1, key2)) in keyset.into_iter().enumerate() {
+    if idx % 3 == 0 {
+      let mut lock_set = LockSet::new(btree, TransactionMode::ReadWrite);
+      BTree::optimistic_insert(btree, &mut lock_set, &key1);
+      BTree::optimistic_insert(btree, &mut lock_set, &key2);
 
-    if !BTree::contains_key(&mut lock_set, &insertion) {
-      println!("Dropped key: {}", insertion);
-      continue;
-    }
+      let key1_present = BTree::contains_key(&mut lock_set, &key1);
+      let key2_present = BTree::contains_key(&mut lock_set, &key2);
+      if !key1_present || !key2_present {
+        println!("A key wasn't inserted?");
+      }
+    } else if idx % 3 == 1 {
+      let mut lock_set = LockSet::new(btree, TransactionMode::ReadWrite);
+      BTree::delete(btree, &mut lock_set, &key1);
+      BTree::delete(btree, &mut lock_set, &key2);
 
-    // And interleave deletions.
-    BTree::delete(btree, &mut lock_set, &insertion);
+      let key1_present = BTree::contains_key(&mut lock_set, &key1);
+      let key2_present = BTree::contains_key(&mut lock_set, &key2);
+      if key1_present || key2_present {
+        println!("A key wasn't deleted?");
+      }
+    } else {
+      let mut lock_set = LockSet::new(btree, TransactionMode::ReadOnly);
+      let key1_present = BTree::contains_key(&mut lock_set, &key1);
+      let key2_present = BTree::contains_key(&mut lock_set, &key2);
 
-    if BTree::contains_key(&mut lock_set, &insertion) {
-      println!("Key was retained: {}", insertion);
-      continue;
+      if key1_present != key2_present {
+        println!("Read transaction isolation violated!");
+      }
     }
   }
 }
