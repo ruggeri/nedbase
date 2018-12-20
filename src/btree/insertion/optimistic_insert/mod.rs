f278f21e2006540066e2f70f320ef7dd1e3d2ad1
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-20 14:23:43 -0800

    delete old B+ tree insertion code
    now replaced with B-link tree

--- src/btree/insertion/optimistic_insert/mod.rs
+++
@@ -1,8 +0,0 @@
-mod acquire_parent_of_deepest_stable_node;
-mod acquire_write_guard_path;
-mod core;
-
-pub(self) use self::acquire_parent_of_deepest_stable_node::acquire_parent_of_deepest_stable_node;
-pub(self) use self::acquire_write_guard_path::acquire_write_guard_path;
-
-pub use self::core::optimistic_insert;
