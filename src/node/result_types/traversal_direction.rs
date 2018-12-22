db6db7d45875d5b61e11ed4ccbcfd56a6f4e1bd7
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-21 20:45:28 -0800

    add some notes about TraversalDirection

--- src/node/result_types/traversal_direction.rs
+++ src/node/result_types/traversal_direction.rs
@@ -1,4 +1,7 @@
+// This enum is used by InteriorNode/LeafNode to tell the user in which
+// way they should move to move toward a target key.
 pub enum TraversalDirection {
+  // Arrived means we are at the LeafNode with the target key.
   Arrived,
   MoveRight { next_node_identifier: String },
   MoveDown { child_node_identifier: String },
