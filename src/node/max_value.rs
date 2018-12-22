1618980f1f7b19d66bdf7d67495c7646c3f869de
Ned Ruggeri <ruggeri@self-loop.com>
2018-12-21 17:30:52 -0800

    add some TODOs for result_types organization

--- src/node/max_value.rs
+++ src/node/max_value.rs
@@ -1,3 +1,6 @@
+// Used in the B-Link tree InteriorNodes to know the max value that can
+// be stored in the subtree (specifically, the rightmost branch). That
+// can tell us when to move to the right, versus descending.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum MaxValue {
   Infinity,
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -9,8 +9,11 @@ mod util;
 pub use self::base_node::Node;
 pub use self::interior_node::InteriorNode;
 pub use self::leaf_node::LeafNode;
+// TODO: move validation code into here. MaxValue only public for
+// validation code... Also, move it into result_types submodule.
 pub use self::max_value::MaxValue;
 pub use self::result_types::{
   DeletionResult, InsertionResult, SplitInfo,
 };
+// TODO: Move TraversalDirection into result_types?
 pub use self::traversal_direction::TraversalDirection;
